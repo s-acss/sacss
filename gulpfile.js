b1ee473d484a53dd9627ae08efe8446b587d7f94
夜见(郑成忠) <yami@xiaohongshu.com>
2021-08-24 13:53:33 +0800

    feat: 添加stylus

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
+var stylus = require('gulp-stylus');
 
 const core = function() {
   return src('./src/index.less')
@@ -40,16 +41,30 @@ const demoScss = function() {
     .pipe(dest('./demo/'));
 };
 
+const demoStylus = function() {
+    return src('./demo/stylus.styl')
+        .pipe(stylus())
+        .pipe(autoprefixer())
+        .pipe(
+            cleanCSS({
+                format: 'keep-breaks',
+            }),
+        )
+        .pipe(dest('./demo/'));
+};
+
 const watchFile = function() {
   // less/demo.less 有引用 css/core.css 文件，所以需要监听
   watch('./less/*.less', demoLess);
   watch('./sass/*.scss', demoScss);
-  watch('./src/index.scss', series(core, demoScss, demoLess));
+  watch('./stylus/*.styl', demoStylus);
+  watch('./src/index.scss', series(core, demoScss, demoLess, demoStylus));
 
   // demo
   watch('./demo/scss.scss', demoScss);
   watch('./demo/less.less', demoLess);
+  watch('./demo/stylus.styl', demoStylus);
 };
 
 exports.default = series(core, demoLess, demoScss, watchFile);
-exports.build = series(core, demoLess, demoScss);
+exports.build = series(core, demoLess, demoScss, demoStylus);
